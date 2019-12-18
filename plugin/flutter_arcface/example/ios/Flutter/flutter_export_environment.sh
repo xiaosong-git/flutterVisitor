200d827f3014159c7a533dd69c56bd672f612a89
hwk <wenkun97@126.com>
2019-12-18 17:25:01 +0800

    移除虹软人脸插件

--- plugin/flutter_arcface/example/ios/Flutter/flutter_export_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\server\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\GIT\xiaosong-master\plugin\flutter_arcface\example"
-export "FLUTTER_TARGET=lib\main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\server\flutter\bin\cache\artifacts\engine\ios"
--- plugin/flutter_arcface/test/flutter_arcface_test.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_arcface/flutter_arcface.dart';
-
-void main() {
-  const MethodChannel channel = MethodChannel('flutter_arcface');
-}
